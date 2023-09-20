db20d1f95a544bb775f8fc997f47260e17490437
jackliuyc <46500003+jackliuyc@users.noreply.github.com>
2023-09-20 16:35:36 -0400

    Edited theme_Publication()
    - Edited title position
    - No bold on axes labels
    - Change font size scaling
    - Legend on left
    - Vertical legend

--- ggplot_theme_Publication-2.R
+++ ggplot_theme_Publication-2.R
@@ -1,37 +1,70 @@
 
 
-theme_Publication <- function(base_size=14, base_family="Helvetica") {
-      library(grid)
-      library(ggthemes)
-      (theme_foundation(base_size=base_size, base_family=base_family)
-       + theme(plot.title = element_text(face = "bold",
-                                         size = rel(1.2), hjust = 0.5, margin = margin(0,0,20,0)),
-               text = element_text(),
-               panel.background = element_rect(colour = NA),
-               plot.background = element_rect(colour = NA),
-               panel.border = element_rect(colour = NA),
-               axis.title = element_text(face = "bold",size = rel(1)),
-               axis.title.y = element_text(angle=90,vjust =2),
-               axis.title.x = element_text(vjust = -0.2),
-               axis.text = element_text(), 
-               axis.line.x = element_line(colour="black"),
-               axis.line.y = element_line(colour="black"),
-               axis.ticks = element_line(),
-               panel.grid.major = element_line(colour="#f0f0f0"),
-               panel.grid.minor = element_blank(),
-               legend.key = element_rect(colour = NA),
-               legend.position = "bottom",
-               legend.direction = "horizontal",
-               legend.box = "vetical",
-               legend.key.size= unit(0.5, "cm"),
-               #legend.margin = unit(0, "cm"),
-               legend.title = element_text(face="italic"),
-               plot.margin=unit(c(10,5,5,5),"mm"),
-               strip.background = element_blank(),
-               strip.text = element_text(face="bold")
-       ))
-      
+theme_Publication <- function(base_size=14, base_family="Arial") {
+   library(grid)
+   library(ggthemes)
+   (theme_foundation(base_size=base_size, base_family=base_family)
+      + theme(plot.title = element_text(face = "bold",
+                                        size = rel(1), hjust = 0.5, margin = margin(0,0,10,0)),
+              text = element_text(),
+              panel.background = element_rect(colour = NA),
+              plot.background = element_rect(colour = NA),
+              panel.border = element_rect(colour = NA),
+              axis.title = element_text(face = "plain",size = rel(1)),
+              axis.title.y = element_text(angle=90,vjust =2),
+              axis.title.x = element_text(vjust = -0.2),
+              axis.text = element_text(), 
+              axis.line.x = element_line(colour="black"),
+              axis.line.y = element_line(colour="black"),
+              axis.ticks = element_line(),
+              panel.grid.major = element_line(colour="#f0f0f0"),
+              panel.grid.minor = element_blank(),
+              legend.key = element_rect(colour = NA),
+              legend.position = "right",
+              legend.direction = "vertical",
+              legend.box = "vetical",
+              legend.key.size= unit(0.5, "cm"),
+              legend.title = element_text(face="plain"),
+              plot.margin=unit(c(10,5,5,5),"mm"),
+              strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+              strip.text = element_text(face="bold")
+      ))
+   
 }
+
+
+# theme_Publication <- function(base_size=14, base_family="Helvetica") {
+#       library(grid)
+#       library(ggthemes)
+#       (theme_foundation(base_size=base_size, base_family=base_family)
+#        + theme(plot.title = element_text(face = "bold",
+#                                          size = rel(1.2), hjust = 0.5, margin = margin(0,0,20,0)),
+#                text = element_text(),
+#                panel.background = element_rect(colour = NA),
+#                plot.background = element_rect(colour = NA),
+#                panel.border = element_rect(colour = NA),
+#                axis.title = element_text(face = "bold",size = rel(1)),
+#                axis.title.y = element_text(angle=90,vjust =2),
+#                axis.title.x = element_text(vjust = -0.2),
+#                axis.text = element_text(), 
+#                axis.line.x = element_line(colour="black"),
+#                axis.line.y = element_line(colour="black"),
+#                axis.ticks = element_line(),
+#                panel.grid.major = element_line(colour="#f0f0f0"),
+#                panel.grid.minor = element_blank(),
+#                legend.key = element_rect(colour = NA),
+#                legend.position = "bottom",
+#                legend.direction = "horizontal",
+#                legend.box = "vetical",
+#                legend.key.size= unit(0.5, "cm"),
+#                #legend.margin = unit(0, "cm"),
+#                legend.title = element_text(face="italic"),
+#                plot.margin=unit(c(10,5,5,5),"mm"),
+#                strip.background = element_blank(),
+#                strip.text = element_text(face="bold")
+#        ))
+#       
+# }
 
 scale_fill_Publication <- function(...){
       library(scales)
